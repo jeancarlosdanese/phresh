a0b4b4e8632113fc9e0db34d7a407a7f49c5e8bc
Jean Carlos Danese <jean@danese.com.br>
2021-04-26 23:10:47 -0300

    Configuring a PoostgreSQL DB with your dockerized FastAPI APP and Migrations with SQL Alchemy and Alembic

---
+++ backend/app/db/migrations/versions/1cc5dd49d51c_create_extension_uuid.py
@@ -0,0 +1,32 @@
+"""create_extension_uuid
+
+Revision ID: 1cc5dd49d51c
+Revises: 
+Create Date: 2021-04-27 00:46:14.693316
+
+"""
+from alembic import op
+import sqlalchemy as sa
+from sqlalchemy.orm import sessionmaker
+
+
+Session = sessionmaker()
+
+
+# revision identifiers, used by Alembic
+revision = '1cc5dd49d51c'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    bind = op.get_bind()
+    session = Session(bind=bind)
+    session.execute("CREATE EXTENSION IF NOT EXISTS \"uuid-ossp\";")
+
+
+def downgrade() -> None:
+    bind = op.get_bind()
+    session = Session(bind=bind)
+    session.execute("DROP EXTENSION IF EXISTS \"uuid-ossp\";")
